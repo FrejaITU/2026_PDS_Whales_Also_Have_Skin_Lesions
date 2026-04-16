c7581bba41e3931b843a1517ab33373e0a80a943
CapperTheRose16 <egcr@itu.dk>
2026-04-16 11:15:49 +0200

    Create mask_components.py

---
+++ src/mask_components.py
@@ -0,0 +1,8 @@
+### Mask components
+import numpy as np
+from skimage.measure import label
+
+
+def mask_components(image, mask):
+    _, num = label(mask, connectivity=2, return_num=True)
+    return num
