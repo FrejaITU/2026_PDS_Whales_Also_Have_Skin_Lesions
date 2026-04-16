75463699ba5dc0c3587fc00a21e56f6b7bbf4640
CapperTheRose16 <egcr@itu.dk>
2026-04-16 12:54:23 +0200

    Update mask_components.py

--- src/mask_components.py
+++ src/mask_components.py
@@ -4,5 +4,8 @@ from skimage.measure import label
 
 
 def mask_components_score(image, mask):
+    if not np.any(mask):
+        return np.nan
+
     _, num = label(mask, connectivity=2, return_num=True)
     return num
