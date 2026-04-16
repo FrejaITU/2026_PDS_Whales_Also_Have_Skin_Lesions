cb773689d94793f190010a20972b40a251cc8887
CapperTheRose16 <egcr@itu.dk>
2026-04-16 11:24:48 +0200

    Update mask_components.py

--- src/mask_components.py
+++ src/mask_components.py
@@ -3,6 +3,6 @@ import numpy as np
 from skimage.measure import label
 
 
-def mask_components(image, mask):
+def mask_components_score(image, mask):
     _, num = label(mask, connectivity=2, return_num=True)
     return num
