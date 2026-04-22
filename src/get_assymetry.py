be79fce2fedea0a8932c9c2eb1e71756080cc4b2
Dfuglen <daniel.fugl@outlook.dk>
2026-04-22 10:00:10 +0200

    Create get_assymetry.py
    Simple assymetry score taken from lecture 5

---
+++ src/get_assymetry.py
@@ -0,0 +1,32 @@
+import numpy as np
+from skimage import morphology
+from skimage.transform import rotate
+
+def get_asymmetry(mask):
+    # mask = color.rgb2gray(mask)
+    scores = []
+    for _ in range(6):
+        segment = crop(mask)
+        (np.sum(segment))
+        scores.append(np.sum(np.logical_xor(segment, np.flip(segment))) / (np.sum(segment)))
+        mask = rotate(mask, 30)
+    avg_score = sum(scores) / len(scores)
+    return {
+         "avg_assymetry_score": avg_score
+    }
+
+def crop(mask):
+        mid = midpointGroup4(mask)
+        y_nonzero, x_nonzero = np.nonzero(mask)
+        y_lims = [np.min(y_nonzero), np.max(y_nonzero)]
+        x_lims = np.array([np.min(x_nonzero), np.max(x_nonzero)])
+        x_dist = max(np.abs(x_lims - mid))
+        x_lims = [mid - x_dist, mid+x_dist]
+        return mask[y_lims[0]:y_lims[1], x_lims[0]:x_lims[1]]
+
+def midpointGroup4(mask):
+        summed = np.sum(mask, axis=0)
+        half_sum = np.sum(summed) / 2
+        for i, n in enumerate(np.add.accumulate(summed)):
+            if n > half_sum:
+                return i
