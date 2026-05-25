15df4552de3875257ac46f6d0097de0de469bf41
Gags50 <125014057+Gags50@users.noreply.github.com>
2026-05-25 14:38:03 +0200

    fixed asymmetry feature extractors

---
+++ src/Features/new_get_asymmetry.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import numpy as np
+from skimage.measure import regionprops
+from skimage.transform import rotate
+import matplotlib.pyplot as plt
+
+mask = plt.imread("../data/masks_biggest_component/PAT_1855_3641_327_mask.png") > 0
+
+def get_asymmetry(mask):
+    mask = np.asarray(mask) > 0
+    mask = resize_mask(mask)
+    mask = center_mask(mask)
+
+    scores = []
+
+    for _ in range(6):
+        plt.imshow(mask)
+        plt.show()
+
+        if segment is not None:
+            flipped = np.fliplr(mask)
+
+            difference = np.logical_xor(mask, flipped)
+            union = np.logical_or(mask, flipped)
+
+            score = np.sum(difference) / np.sum(union)
+            scores.append(score)
+
+        mask = rotate(mask, 30, order=0) > 0.5
+
+    if len(scores) == 0:
+        return {
+            "avg_asymmetry_score": np.nan,
+            "worst_score": np.nan
+        }
+
+    return {
+        "avg_asymmetry_score": sum(scores) / len(scores),
+        "worst_score": max(scores)
+    }
+
+
+def crop(mask):
+    y, x = np.nonzero(mask)
+
+    if len(y) == 0:
+        return None
+
+    return mask[y.min():y.max() + 1, x.min():x.max() + 1]
+
+
+def resize_mask(mask):
+    mask = crop(mask)
+    h, w = mask.shape
+    hw = max((h, w))*2
+    extra = np.zeros((hw, hw))
+    extra[0:h, 0:w] = mask
+    
+    return(extra)
+
+def center_mask(mask):
+    props = regionprops(mask.astype(int))[0]
+    Yc, Xc = props.centroid
+
+
+    cy, cx = np.array(mask.shape) / 2
+
+    shift_y = cy - Yc
+    shift_x = cx - Xc
+
+    centered = shift(mask, shift=(shift_y, shift_x), order=0)
+
+    return(centered > 0)
+
+
+get_asymmetry(mask)
+
+
+# In[ ]:
+
+
+
+
---
+++ src/Features/new_mabrouk.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+from skimage.measure import regionprops
+from skimage.segmentation import find_boundaries
+from skimage.transform import rotate
+from scipy.ndimage import shift
+import matplotlib.pyplot as plt
+
+mask = plt.imread("../data/masks_biggest_component/PAT_1855_3641_327_mask.png")
+mask = mask > 0
+
+def crop(mask):
+    y, x = np.nonzero(mask)
+
+    if len(y) == 0:
+        return None
+
+    return mask[y.min():y.max() + 1, x.min():x.max() + 1]
+
+def resize_mask(mask):
+    mask = crop(mask)
+    h, w = mask.shape
+    hw = max((h, w))*2
+    extra = np.zeros((hw, hw))
+    extra[0:h, 0:w] = mask
+    
+    return(extra)
+
+def center_mask(mask):
+    props = regionprops(mask.astype(int))[0]
+    Yc, Xc = props.centroid
+
+
+    cy, cx = np.array(mask.shape) / 2
+
+    shift_y = cy - Yc
+    shift_x = cx - Xc
+
+    centered = shift(mask, shift=(shift_y, shift_x), order=0)
+
+    return(centered > 0)
+
+def Mabrouk_find_axis(mask):
+    """Finds the axis at which the boundary is closest to the centroid,
+    as done in the 2020 Mabrouk method of finding asymmetry score"""
+
+    #Find the centroid of our mask
+    props = regionprops(mask.astype(int))[0]
+    Yc, Xc = props.centroid
+
+    #Extract boundary pixels
+    boundary = find_boundaries(mask, mode='outer')
+    ys, xs = np.where(boundary)
+
+    #vector from centroid to each boundary
+    dy = ys - Yc
+    dx = xs - Xc
+
+    #angles and distance of each boundary pixel
+    angles = np.arctan2(dy, dx)
+    angles_deg = (np.degrees(angles) +360) % 360
+
+    distance = np.sqrt(dx*dx + dy*dy)
+
+    #binning by angle and keeping the closest boundary
+    bins = np.arange(360)
+    min_dist = np.full(360, np.inf)
+
+    for a, d in zip(angles_deg.astype(int), distance):
+        if d < min_dist[a]:
+            min_dist[a] = d
+    
+    #Extracting the axis with the closest boundary
+    closest_axes = np.where(min_dist == np.min(min_dist))[0]
+
+    return closest_axes
+
+def reflect_mask(mask, angle_deg):
+    """Reflects the mask across an axis passing through the centroid at angle_deg"""
+
+    #Rotate so axis becomes vertical
+    rot = rotate(mask, -angle_deg, order=0, preserve_range=True)
+    rot = rot >0.5
+
+    #reflect horizontally
+    refl = np.fliplr(rot)
+
+    #rotate back
+    refl_back = rotate(refl, angle_deg, order=0, preserve_range=True)
+    refl_back = refl_back > 0.5
+
+    return refl_back
+
+def delta_A(mask, reflected):
+    """Computes the non-overlapping area"""
+
+    xor = np.logical_xor(mask, reflected)
+    plt.imshow(xor)
+    plt.show()
+    return xor.sum()
+
+def Mabrouk_asymmetry(mask):
+    """Computes the asymmetry score using the method from the 2020 Mabrouk research paper on skin lesion diagnosis"""
+    mask = resize_mask(mask)
+    mask = center_mask(mask)
+
+    #Find axis closest to centroid
+    axis1 = int(Mabrouk_find_axis(mask)[0])
+    axis2 = (axis1 + 90) % 360
+
+    #Reflect across both axes
+    refl1 = reflect_mask(mask, axis1)
+    refl2 = reflect_mask(mask, axis2)
+
+    #compute delta A for each axis
+    A = mask.sum()
+    dA1 = delta_A(mask, refl1)
+    dA2 = delta_A(mask, refl2)
+
+    #Compute asymmetry index for each axis
+    AI1 = dA1 / A
+    AI2 = dA2 / A
+
+    #Final asymmetry score (0, 1, or 2)
+    score = int(AI1 > 1) + int(AI2 > 1)
+
+    return {
+        "mabrouk_asymmetry_score": score
+    }
+
+Mabrouk_asymmetry(mask)
+
+
+# In[ ]:
+
+
+
+
