8cc1e374ae3b01353bf2eb4f063efc8b0501d2b8
CapperTheRose16 <egcr@itu.dk>
2026-05-25 11:54:54 +0200

    Create get_compactness.py

---
+++ src/Features/get_compactness.py
@@ -0,0 +1,17 @@
+import numpy as np
+from skimage.measure import perimeter
+
+def get_compactness(mask):
+    """Compute Polsby-Popper compactness score."""
+    
+    area = np.sum(mask)
+    perim = perimeter(mask)
+    
+    if perim == 0:
+        return {"Polsby-Popper": 0}
+    
+    compact = (4 * np.pi * area) / (perim ** 2)
+    
+    return {
+        "Polsby-Popper": compact
+}
